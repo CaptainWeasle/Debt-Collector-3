70921795f3ebe5aee4be1d0d34da19e303fa5e54
CaptainWeasle <voroschilin.andre@outlook.de>
2020-06-02 13:12:43 +0200

    added colorprofile repo implementation

---
+++ lib/features/color_profiles/data/repositories/color_profile_repository_implementation.dart
@@ -0,0 +1,34 @@
+import 'package:debt_collector_3/features/color_profiles/domain/entities/color_profile.dart';
+import 'package:debt_collector_3/features/color_profiles/domain/repositories/color_profile_repository.dart';
+
+class ColorProfileRepositoryImpl extends ColorProfileRepository {
+  @override
+  Future addColorProfile(ColorProfile colorProfile) {
+    // TODO: implement addColorProfile
+    return null;
+  }
+
+  @override
+  Future deleteAllColorProfiles() {
+    // TODO: implement deleteAllColorProfiles
+    return null;
+  }
+
+  @override
+  Future deleteColorProfile(ColorProfile colorProfile) {
+    // TODO: implement deleteColorProfile
+    return null;
+  }
+
+  @override
+  Future<List<ColorProfile>> getAllColorProfiles() {
+    // TODO: implement getAllColorProfiles
+    return null;
+  }
+
+  @override
+  Future updateColorProfile(ColorProfile colorProfile) {
+    // TODO: implement updateColorProfile
+    return null;
+  }
+}
