7eab8fab62a4cb90a413d48037c7aef692330014
CaptainWeasle <voroschilin.andre@outlook.de>
2020-06-05 16:12:43 +0200

    added color profile feature

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,14 +1,9 @@
-import 'package:debt_collector_3/database/moor_database/moor_database.dart';
-import 'package:debt_collector_3/features/debts/data/datasources/debt_database_datasource.dart';
-import 'package:debt_collector_3/features/debts/data/repositories/debt_repository_impl.dart';
-import 'package:debt_collector_3/features/debts/domain/repositories/debt_repository.dart';
-import 'package:debt_collector_3/features/debts/presentation/bloc/debt_bloc.dart';
-import 'package:debt_collector_3/features/debts/domain/usecases/delete_completed_debts.dart'
-    as prefix5;
 import 'package:debt_collector_3/features/debts/domain/usecases/add_debt.dart'
     as prefix4;
 import 'package:debt_collector_3/features/debts/domain/usecases/delete_all_debts.dart'
     as prefix3;
+import 'package:debt_collector_3/features/debts/domain/usecases/delete_completed_debts.dart'
+    as prefix5;
 import 'package:debt_collector_3/features/debts/domain/usecases/delete_debt.dart'
     as prefix0;
 import 'package:debt_collector_3/features/debts/domain/usecases/get_all_debts.dart'
@@ -18,21 +13,37 @@ import 'package:debt_collector_3/features/debts/domain/usecases/update_debt.dart
 import 'package:get_it/get_it.dart';
 
 import 'database/dao/debt_feature/debt_dao.dart';
+import 'database/moor_database/moor_database.dart';
+import 'features/color_profiles/data/datasources/color_profile_datasource.dart';
+import 'features/color_profiles/data/repositories/color_profile_repository_implementation.dart';
+import 'features/color_profiles/domain/repositories/color_profile_repository.dart';
+import 'features/color_profiles/domain/usecases/add_color_profile.dart';
+import 'features/color_profiles/domain/usecases/delete_all_color_profiles.dart';
+import 'features/color_profiles/domain/usecases/delete_color_profile.dart';
+import 'features/color_profiles/domain/usecases/get_all_color_profiles.dart';
+import 'features/color_profiles/domain/usecases/update_color_profile.dart';
+import 'features/color_profiles/presentation/bloc/color_profile_bloc.dart';
+import 'features/debts/data/datasources/debt_database_datasource.dart';
+import 'features/debts/data/repositories/debt_repository_impl.dart';
+import 'features/debts/domain/repositories/debt_repository.dart';
+import 'features/debts/presentation/bloc/debt_bloc.dart';
 
 // sl steht für service locator
 final sl = GetIt.instance;
 
 void init() {
   //! Features - Debt
-  // Bloc (factory weil man streams closen muss)
-  sl.registerFactory(() => DebtBloc(
-        all: sl(),
-        add: sl(),
-        update: sl(),
-        delete: sl(),
-        deleteA: sl(),
-        deleteC: sl(),
-      ));
+  // Bloc (factory weil man streams closen muss
+  sl.registerFactory(
+    () => DebtBloc(
+      all: sl(),
+      add: sl(),
+      update: sl(),
+      delete: sl(),
+      deleteA: sl(),
+      deleteC: sl(),
+    ),
+  );
 
   // Usecases
   sl.registerLazySingleton(() => prefix2.GetAllDebts(sl()));
@@ -43,13 +54,45 @@ void init() {
   sl.registerLazySingleton(() => prefix5.DeleteCompletedDebts(sl()));
 
   // Repository
-  sl.registerLazySingleton<DebtRepository>(() => DebtRepositoryImpl(
-        databaseDatasource: sl(),
-      ));
+  sl.registerLazySingleton<DebtRepository>(
+    () => DebtRepositoryImpl(
+      databaseDatasource: sl(),
+    ),
+  );
 
   // Datasources
   sl.registerLazySingleton<DebtDatabaseDataSource>(
       () => DebtDatabaseDataSourceImpl(dao: sl()));
+
+  //! Festures ColorProfile
+  // Bloc (factory weil man streams closen muss
+  sl.registerFactory(
+    () => ColorProfileBloc(
+      all: sl(),
+      add: sl(),
+      update: sl(),
+      delete: sl(),
+      deleteAll: sl(),
+    ),
+  );
+
+  // Usecases
+  sl.registerLazySingleton(() => GetAllProfiles(repository: sl()));
+  sl.registerLazySingleton(() => AddColorProfile(repository: sl()));
+  sl.registerLazySingleton(() => UpdateColorProfile(repository: sl()));
+  sl.registerLazySingleton(() => DeleteColorProfile(repository: sl()));
+  sl.registerLazySingleton(() => DeleteAllColorProfiles(repository: sl()));
+
+  // Repository
+  sl.registerLazySingleton<ColorProfileRepository>(
+    () => ColorProfileRepositoryImpl(
+      databaseDatasource: sl(),
+    ),
+  );
+
+  // Datasources
+  sl.registerLazySingleton<ColorProfileDatasource>(
+      () => ColorProfileDatasourceImpl(dao: sl()));
 
   //! Core
 
